cb055f1eab177c32c6b5c06bb82866e00ba8afc3
Afshar <afshartech@gmail.com>
2015-03-22 14:14:46 +0530

    Generator will not do anything when some error occurs while retrieving DB tables data

--- src/com/afshar/cg/CodeGenerator.java
+++ src/com/afshar/cg/CodeGenerator.java
@@ -44,34 +44,40 @@ public class CodeGenerator
 			// Retrieve all tables & their columns and create a corresponding list of java objects
 			List<CGTable> cgTables = CGDatabaseManager.getTables( properties );
 			
-			// Using the 'java objects' for sql tables, create a 'CodeGenerator.xml', to enable user editing
-			boolean fileCreated = CGFileManager.createCGXML( CODE_GENERATION_PATH, cgTables );
-			
-			if(fileCreated)
+			if(cgTables.isEmpty())
 			{
-				readUserInput(); // If any exception is occurred here.. program with close.
-				
-				System.out.println( "<<Parsing CodeGeneration.xml>>" );
-				 
-				// Parse CodeGeneration.xml file & create new List of CGTable objects from it 
-				List<CGTable> verifiedCGTables = CGFileManager.readCGXMLAndGetTables( CODE_GENERATION_PATH );
-				
-				
-				String s = CODE_GENERATION_PATH.replaceAll( Pattern.quote( "/" ),"//" )
-						+ CODE_GENERATION_BASE_PKG.replaceAll( Pattern.quote( "." ),"//" ); //Windows only 
-
-				// Create required directory structure 
-				CGFileManager.createDirectories(s);
-				
-				// Scan & Parse all template files & generate code as per template file using data from CodeGeneration.xml
-				CGFileScanner.scanTemplateFiles( properties, verifiedCGTables, s );
-				
+				System.err.println("Some error occured while loading DB table data.");
 			}
 			else 
 			{
-				System.out.println("Error occured while generating code. Please check logs.");
+				// Using the 'java objects' for sql tables, create a 'CodeGenerator.xml', to enable user editing
+				boolean fileCreated = CGFileManager.createCGXML( CODE_GENERATION_PATH, cgTables );
+				
+				if(fileCreated)
+				{
+					readUserInput(); // If any exception is occurred here.. program with close.
+					
+					System.out.println( "<<Parsing CodeGeneration.xml>>" );
+					 
+					// Parse CodeGeneration.xml file & create new List of CGTable objects from it 
+					List<CGTable> verifiedCGTables = CGFileManager.readCGXMLAndGetTables( CODE_GENERATION_PATH );
+					
+					
+					String s = CODE_GENERATION_PATH.replaceAll( Pattern.quote( "/" ),"//" )
+							+ CODE_GENERATION_BASE_PKG.replaceAll( Pattern.quote( "." ),"//" ); //Windows only 
+
+					// Create required directory structure 
+					CGFileManager.createDirectories(s);
+					
+					// Scan & Parse all template files & generate code as per template file using data from CodeGeneration.xml
+					CGFileScanner.scanTemplateFiles( properties, verifiedCGTables, s );
+					
+				}
+				else 
+				{
+					System.out.println("Error occured while generating code. Please check logs.");
+				}
 			}
-			
 			
 		}
 		catch (IOException e){
